cf950f133b69bf8c7fd6119ae7a04ea5234092a8
MukulRana31 <mukul.ginilytics@gmail.com>
2024-01-29 17:21:52 +0530

    resize

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-
+const hostname = "192.168.10.118";
+const port = 3000;
 app.get('/Product', (req, res) => {
 
     res.json({
@@ -27,4 +28,7 @@ app.use(express.static(__dirname));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
 
-app.listen(3000, () => console.log('Bot start on port 3000'));
+//app.listen(3000, () => console.log('Bot start on port 3000'));
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
