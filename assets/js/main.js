a2f5e30490e5d391f05e4cd540f7f14680b7b933
MukulRana31 <mukul.ginilytics@gmail.com>
2024-01-31 17:01:40 +0530

    custom input

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,82 @@
 "use-strict"
 
 var convo;
+const deepClone = (_ele) => {
+    if (_ele instanceof HTMLElement) {
+        const cloned = _ele.cloneNode();
 
+        _ele.childNodes.forEach(ch => ch instanceof Text && cloned.append(ch.cloneNode()) || ch instanceof HTMLElement && cloned.append(
+            deepClone(ch)
+        ));
+
+        return cloned;
+    }
+}
+
+
+const inputState =  {
+    inputVal:[]
+}
+ var userinput;
 // load json file with formless data
 window.onload = () => {
     loadJSON("main", res => startConversation(res))
+
+    setTimeout(_=>{
+debugger;
+     
+
+        Array.prototype.forEach.bind(document.getElementsByTagName("textarea"))(
+
+            txta=>{
+                  var cl = txta.cloneNode();
+                  txta.parentNode.replaceChild(cl,txta);
+
+                  cl.addEventListener("keydown",function(e){ 
+
+                    if(e.key.toLocaleLowerCase() =="backspace") {
+                        inputState.inputVal.pop();
+                    }  else{
+                        if(!(e.key.length>2)){
+
+                            inputState.inputVal.push(e.key);
+                        }
+                    }     
+              })
+            }
+        )
+        var originalElement = document.getElementsByTagName('cf-input-button');
+
+        Array.prototype.forEach.bind(originalElement)(
+            ele=>{
+                var clonedElement = deepClone(ele);
+                ele.parentNode.replaceChild(clonedElement, ele);
+                clonedElement.addEventListener("click", function () {
+                    
+                    var inputValue = inputState.inputVal.toString().replace(/,/g, '');
+                    console.log(inputValue);
+                
+                   
+                    submitAndLoadBranch(inputValue);
+                });
+            
+            }
+        );
+    },1000);
+}
+function submitAndLoadBranch(inputValue) {
+    debugger;
+    if (inputValue.toLowerCase().includes('product')) {
+        
+        userinput = 'opt-Product-category';
+    }
+    loadBranch(userinput, (succ) => {
+        if (!succ) {
+            console.error("Failed to load branch");
+        }
+    });
+    
+    convo.addUserChatResponse(inputValue);
 }
 
 function startConversation(json) {
@@ -36,9 +108,11 @@ let onStepCallback = function (dto, success, error) {
         success()
         return
     }
+    debugger;
 
     let cond = dto.tag._values[0]
     console.log("Loading branch... " + cond)
+  
     loadBranch(cond, (succ) => {
         if (!succ) {
             error()
@@ -49,6 +123,7 @@ let onStepCallback = function (dto, success, error) {
 }
 
 function loadBranch(branch, callback) {
+    debugger;
     loadJSON(branch, (json) => {
         if (!json) {
             callback(false)
@@ -62,8 +137,7 @@ function loadBranch(branch, callback) {
 
 // Loads JSON chat file
 function loadJSON(name, callback) {
-
-
+debugger;
     let xhr = new XMLHttpRequest()
     xhr.overrideMimeType('application/json')
     xhr.onload = () => {
@@ -79,6 +153,11 @@ if(name.match(/opt|main/g)&&name.match(/opt|main/g).length>0){
     xhr.open("GET", window.location.origin+'/'+name )
     xhr.send(null)
 }
+
+
+
+
+
 
 
 
